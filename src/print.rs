b48c2c10713091315b56a173d8f445b5a528f856
Brian De Maio <bdemaio1@gmail.com>
2020-07-07 00:18:11 -0400

    More string println argument template syntax

--- src/print.rs
+++ src/print.rs
@@ -3,5 +3,18 @@ pub fn run() {
     //Print to console
     println!("Hello, from the print.rs file");
     //String literal
-    println!("Number: {}", 1);
+    println!("Number: {} and {}", 1, 2);
+
+    //Positional Arguments
+    println!(
+        "{0} is from {1} and {0} likes to {2}",
+        "Beau", "Florida", "code"
+    );
+
+    //Named Arguments
+    println!(
+        "{name} likes to play {activity}",
+        name = "Beau",
+        activity = "Baseball"
+    );
 }
