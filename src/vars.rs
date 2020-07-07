6761198c6f80fb6927c5f39b68673261c5be05ca
Brian De Maio <bdemaio1@gmail.com>
2020-07-07 00:38:43 -0400

    Running into types

--- src/vars.rs
+++ src/vars.rs
@@ -10,4 +10,13 @@ pub fn run() {
 
     age = 31;
     println!("My name is {} and I am {}", name, age);
+
+    //Define constant
+    //i32 - 32 bit interger type
+    const ID: i32 = 001;
+    println!("ID: {}", ID);
+
+    //Assign multiple variables
+    let (my_name, my_age) = ("Beau", 31);
+    println!("{} is {}", my_name, my_age);
 }
