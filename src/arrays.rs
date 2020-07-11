54e2bbb9c974ce881c2f7ab4ce8abf54e8ece8ec
Brian De Maio <bdemaio1@gmail.com>
2020-07-11 00:04:26 -0400

    Tuples and arrays

--- src/arrays.rs
+++ src/arrays.rs
@@ -9,9 +9,19 @@ pub fn run() {
     //let numbers: [i32; 5] = [1, 2, 3, 4, ""];
 
     //[type; length]
-    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+    let mut numbers: [i32; 5] = [1, 2, 3, 4, 5];
 
+    //Re-assign value
+    numbers[2] = 20;
+
+    //Single value
     println!("Single value: {}", numbers[0]);
+
+    //Get array length
+    println!("Array length: {}", numbers.len());
+
+    //Get amount of memeory allocated in stack
+    println!("Array mem usage: {}", std::mem::size_of_val(&numbers));
 
     println!("{:?}", numbers);
 }
