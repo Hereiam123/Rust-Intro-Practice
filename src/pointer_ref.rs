e22671a264d93ff913ef894dbd673f6bd3d457ef
Brian De Maio <bdemaio1@gmail.com>
2020-07-12 18:08:34 -0400

    Reference and pointer

--- src/pointer_ref.rs
+++ src/pointer_ref.rs
@@ -1,1 +1,16 @@
 //Reference Pointers - Point to a resource in memory
+
+pub fn run() {
+    //Primitive array
+    let arr1 = [1, 2, 3];
+    let arr2 = arr1;
+
+    //Non-primitve, if you assign another
+    //variable to a piece of data, the
+    //first variable will no longer hold the value,
+    //you need to use a reference (&) to point to the resource
+    let vec1 = vec![1, 2, 3];
+    let vec2 = &vec1;
+
+    println!("Values: {:?}", (&vec1, vec2));
+}
